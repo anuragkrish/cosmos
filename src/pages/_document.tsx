8a2ce08a1f7d5978710fca0b0681b5cf55908f18
anuragkrish <anurag.krishna@headout.com>
2026-06-19 16:39:27 +0530

    fix: font fixes and ui preview fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { InlineFontFace, PreloadFontLinks } from '@headout/eevee';
 
 export default function Document() {
 	return (
 		<Html lang='en'>
-			<Head />
+			<Head>
+				<PreloadFontLinks />
+				<InlineFontFace />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
